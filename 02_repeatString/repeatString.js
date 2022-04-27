31f294aa52f1b84360b3c17a1c40c10c2953828f
baconbroth <jason.chen94@gmail.com>
2022-04-26 23:37:43 -0400

    helloworld, repeatstring completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let finalstring = '';
+if (num<0) {
+    return "ERROR";
+} else {
+    for(let i=0; i<num; i++) {
+        finalstring += string;
+        }
+    return finalstring;
 };
+
+repeatString('hey', 3);    
+}
 
 // Do not edit below this line
 module.exports = repeatString;
