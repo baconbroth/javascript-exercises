97a95a08e420de97583805f060658500fd833136
baconbroth <jason.chen94@gmail.com>
2022-04-29 22:12:36 -0400

    up to js exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return answer = num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return answer = num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+};
+const multiply = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+  return answer = Math.pow(num1, num2);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for(let i = num; i>0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
